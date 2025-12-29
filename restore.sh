e2aaf853d3887a19a01f96f59bddb1b3d0848a60
slashdekomori <slashdekomori@proton.me>
2025-12-29 14:30:31 +0300

    iproved .gitignore and restore.sh

--- restore.sh
+++ restore.sh
@@ -1,6 +1,12 @@
-cd ~/.config
-git init
-git remote set-url origin git remote set-url origin git@github.com:slashdekomori/.config.git
-git fetch origin
-git reset --hard origin/main
+#!/bin/sh
+set -e
 
+cd "$HOME/.config"
+
+if [ ! -d .git ]; then
+  git init
+  git remote add origin git@github.com:slashdekomori/.config.git
+fi
+
+git fetch origin
+git checkout -B main origin/main
